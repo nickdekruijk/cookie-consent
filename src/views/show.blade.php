0a2180f9d303252b73a7ec7e0d3fe544f80049c2
Nick de Kruijk <git@nickdekruijk.nl>
2025-03-17 17:55:18 +0100

    Set autofocus and change order for accessibility

--- src/views/show.blade.php
+++ src/views/show.blade.php
@@ -1,10 +1,10 @@
 @if (!cookieConsentAccepted() && !cookieConsentDenied())
-<div class="cookieconsent-container">
-    <div class="cookieconsent-explain">
-        @lang('cookieconsent::cookieconsent.explain')
+    <div class="cookieconsent-container">
+        <div class="cookieconsent-explain">
+            @lang('cookieconsent::cookieconsent.explain')
+        </div>
+        <a href="{{ route('cookieconsent', 'accept') }}" autofocus class="cookieconsent-button cookieconsent-button-accept">@lang('cookieconsent::cookieconsent.accept')</a>
+        <a href="@lang('cookieconsent::cookieconsent.moreinfo_link')" class="cookieconsent-button cookieconsent-button-moreinfo">@lang('cookieconsent::cookieconsent.moreinfo')</a>
+        <a href="{{ route('cookieconsent', 'deny') }}" class="cookieconsent-close">&times;</a>
     </div>
-    <a href="{{ route('cookieconsent', 'deny') }}" class="cookieconsent-close">&times;</a>
-    <a href="{{ route('cookieconsent', 'accept') }}" class="cookieconsent-button cookieconsent-button-accept">@lang('cookieconsent::cookieconsent.accept')</a>
-    <a href="@lang('cookieconsent::cookieconsent.moreinfo_link')" class="cookieconsent-button cookieconsent-button-moreinfo">@lang('cookieconsent::cookieconsent.moreinfo')</a>
-</div>
 @endif
