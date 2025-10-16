a1105708e9584fcd9d017fb4e8786796c4eba67c
Nick de Kruijk <git@nickdekruijk.nl>
2025-10-16 10:06:32 +0200

    Improve config.php documentation and update cookie settings
    Enhanced comments for clarity and consistency in config.php. Updated 'cookie_secure' to use the SESSION_SECURE_COOKIE environment variable and improved documentation for cookie options.

--- src/config.php
+++ src/config.php
@@ -4,9 +4,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | cookie_name
+    | Consent Cookie Name
     |--------------------------------------------------------------------------
-    | Name of the cookie to use
+    |
+    | Here you may change the name of the consent cookie that is created by
+    | the package. 
+    |
     */
     'cookie_name' => 'cookie_consent',
 
@@ -14,33 +17,44 @@ return [
     |--------------------------------------------------------------------------
     | route_prefix
     |--------------------------------------------------------------------------
+    |
     | Use this route to set the cookie. Default 'cookie_consent' will translate
     | into an url like https://domain.com/cookie_consent/accept or
     | https://domain.com/cookie_consent/deny
+    |
     */
     'route_prefix' => 'cookieconsent',
 
     /*
     |--------------------------------------------------------------------------
-    | cookie_expires
+    | Consent Cookie Expiration
     |--------------------------------------------------------------------------
+    |
     | The cookie 'cookie_name' should last this amount of days
+    |
     */
     'cookie_expires' => 365,
 
     /*
     |--------------------------------------------------------------------------
-    | cookie_secure
+    | HTTPS Only Consent Cookie
     |--------------------------------------------------------------------------
-    | The cookie 'cookie_name' should last this amount of days
+    |
+    | By setting this option to true, cookie consent will only work if the 
+    | browser has a HTTPS connection.
+    |
     */
-    'cookie_secure' => false,
+    'cookie_secure' => env('SESSION_SECURE_COOKIE', false),
 
     /*
     |--------------------------------------------------------------------------
-    | cookie_secure
+    | HTTP Access Only
     |--------------------------------------------------------------------------
-    | The cookie 'cookie_name' should last this amount of days
+    |
+    | Setting this value to true will prevent JavaScript from accessing the
+    | value of the cookie and the cookie will only be accessible through
+    | the HTTP protocol. It's unlikely you should disable this option.
+    |
     */
     'cookie_httponly' => true,
 
